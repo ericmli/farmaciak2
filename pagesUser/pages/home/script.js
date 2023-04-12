7f7ab99f229a480e9ec266ff23203da022bf924a
ericmli <ericmli1334@gmail.com>
2023-04-12 20:28:20 -0300

    Update

--- pagesUser/pages/home/script.js
+++ pagesUser/pages/home/script.js
@@ -14,15 +14,20 @@ function callFunction(){
             console.log(data.result[0].img)
             let htmlLis = ''
             for (let i = 0; i < data.result.length; i++) {
-                htmlLis += `  
+                console.log(data.result[i].status)
+                if(data.result[i].status === 'Ativo'){
+                    htmlLis += `  
                     <li class="product" onclick="callLocal(${data.result[i].id})">
                         <img src="${data.result[i].img}">
                         <div class="infoCard">
                             <p class="textNameCard" >${data.result[i].nome}</p>
+                            <p class="textNameCard" >R$ ${data.result[i].preco}</p>
                             <p class="textCard" >${data.result[i].descricao}</p>
                         </div>
                         </li>
-    `
+                    `
+                }
+                
             }
             document.getElementById('products').innerHTML = htmlLis
         },
