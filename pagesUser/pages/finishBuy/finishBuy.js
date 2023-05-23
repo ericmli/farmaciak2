aea8bcd1aed009201bf153cd316ef9639ba2c324
ericmli <ericmli1334@gmail.com>
2023-05-23 11:09:28 -0300

    update

--- pagesUser/pages/finishBuy/finishBuy.js
+++ pagesUser/pages/finishBuy/finishBuy.js
@@ -87,22 +87,110 @@ function cart() {
     <p> <strong>Subtotal: ${subtotal} + Frete: ${temItem} = R$${Number(subtotal)+ Number(temItem)}</strong> </p>    
     <p> <button type="button" class="btn btn-primary" onclick="removerEnd()">Escolher outro frete</button></p>
     <div class="form-check">
-        <input class="form-check-input" type="radio" name="opcao" id="exampleRadios1"
-            value="${preco1}">
+        <input class="form-check-input" type="radio" name="opcaoCred" id="exampleRadios1"
+        value="Cartão">
         <label class="form-check-label" for="exampleRadios1">
-            SEDEX : R$${preco1}
+            Cartão
         </label>
         </div>
         <div class="form-check">
-        <input class="form-check-input" type="radio" name="opcao" id="exampleRadios2"
-            value="${preco2}">
+        <input class="form-check-input" type="radio" name="opcaoCred" id="exampleRadios2"
+        value="Boleto">
         <label class="form-check-label" for="exampleRadios2">
-            PAC : R$${preco2}
+            Boleto
         </label>
         </div>
       </div>
     `
     document.getElementById('addRadio').innerHTML = soma
+  }
+
+  let tipoPagamento = sessionStorage.getItem('tipoPagamento')
+  let add = ''
+  if(tipoPagamento === 'Cartão'){
+    add = `
+    <form class="mt-5 text-left">
+
+    <div class="form-group row">
+      <label for="staticEmail" class="col-sm-2 col-form-label">Nome</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="inputNome" placeholder="Nome Completo">
+      </div>
+    </div>
+
+
+
+    <div class="form-group row">
+      <label for="inputPassword" class="col-sm-2 col-form-label">Validade</label>
+      <div class="col-sm-10">
+        <input type="password" class="form-control" id="inputPassword" placeholder='01/30'>
+      </div>
+    </div>
+
+
+    <div class="form-group row">
+      <label for="staticEmail" class="col-sm-2 col-form-label">Código de segurança</label>
+      <div class="col-sm-10">
+        <input type="string" class="form-control" id="inputData" placeholder='000'>
+      </div>
+    </div>
+
+    <div class="form-group row">
+      <label for="staticEmail" class="col-sm-2 col-form-label">Número</label>
+      <div class="col-sm-10" >
+        <input type="string" class="form-control" id="inputNumber" placeholder='1234 5678 1234 5678'>
+      </div>
+    </div>
+
+
+  </form>
+      <button type="button" class="btn btn-info m-2" onclick="atualizar()">Comprar</button>
+    `
+    document.getElementById('addPagamento').innerHTML = add
+    let name = document.getElementById("inputNome").value.trim();
+    let password = document.getElementById("inputPassword").value.trim();
+    let date = document.getElementById("inputData").value.trim();
+    let number = document.getElementById("inputNumber").value.trim();
+  
+  
+    if (
+      name.length == 0 ||
+      password == 0 ||
+      date.length == 0
+    ) {
+      if (name.length == 0 || name.length > 99) {
+        document.getElementById("inputNome").classList.add(`error`);
+      } else {
+        document.getElementById("inputNome").classList.remove(`error`);
+      }
+      if (password.length == 0) {
+        document.getElementById("inputPassword").classList.add(`error`);
+      } else {
+        document.getElementById("inputPassword").classList.remove(`error`);
+      }
+  
+      if(date.length == 0){
+          document.getElementById("inputData").classList.add(`error`);
+      }else{
+          document.getElementById("inputData").classList.remove(`error`);
+      }
+  
+      if(number.length == 0 ){
+        document.getElementById("inputNumber").classList.add(`error`);
+      }else{
+        document.getElementById("inputNumber").classList.remove(`error`);
+      }
+    }
+  } else if ( tipoPagamento === 'Boleto'){
+    add = `
+      <img src="../../../src/boletobancario.jpg">
+      <button type="button" class="btn btn-info m-2" onclick="atualizar()">Comprar</button>
+    `
+    document.getElementById('addPagamento').innerHTML = add
+  } else {
+    add = `
+    `
+    document.getElementById('addPagamento').innerHTML = add
   }
   
   var radios = document.getElementsByName('opcao');
@@ -114,6 +202,14 @@ function cart() {
 
     });
   }
+  var radiosCred = document.getElementsByName('opcaoCred');
+  for (var i = 0; i < radiosCred.length; i++) {
+    radiosCred[i].addEventListener('change', function() {
+      var valorSelecionado = this.value;
+      sessionStorage.setItem('tipoPagamento', valorSelecionado);
+      window.location.reload();
+    });
+  }
 
 }
 
@@ -121,4 +217,8 @@ function removerEnd(){
   sessionStorage.clear()
   window.location.reload();
 
+}
+
+function atualizar() { 
+  alert('AA')
 }
